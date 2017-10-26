9a99d93c36fe42dad8724fc471dbab28ad4bf103
Dan Rosén <dan.rosen@gu.se>
2017-10-26 19:28:31 +0200

    Introduce Lens interface

--- src/reactive-lens.ts
+++ src/reactive-lens.ts
@@ -1,25 +1,115 @@
+/** A simple lens */
+export interface Lens<S, T> {
+  get(s: S): T,
+  set(s: S, t: T): S
+}
 
-/** Storing state */
-export class Store<S> {
+/** Store for some state */
+export interface Store<S> {
+  /** Get the current value (which must not be mutated) */
+  get(): S
+
+  /** Set the value */
+  set(s: S): void
+
+  /** Modify the value in the store (must not use mutation: return a new value) */
+  modify(f: (s: S) => S): void
+
+  /** React on changes. Returns an unsubscribe function. */
+  on(k: (s: S) => void): () => void
+
+  /** Start a new transaction: listeners are only invoked when the
+  (top-level) transaction finishes, and not on set (and modify) inside the transaction. */
+  transaction(m: () => void): void
+
+  /** Make a new store by projecting at a subfield.
+
+  Note: use only when S is an object which always has this key. */
+  at<K extends keyof S>(k: K): Store<S[K]>
+
+  /** Make a reference at a particular key in a record.
+
+  Note: the key may be missing from the record.
+  Note: setting the value to undefined removes the key from the record. */
+  key<K extends keyof S>(k: K): Store<S[K] | undefined>
+
+  /** Make a new store by going via a lens */
+  via<T>(lens: Lens<S, T>): Store<T>
+
+  /** Make a substore with respect to some base store */
+  substore<T>(get: () => T, set: (s: T) => void): Store<T>
+}
+
+/** reactive-lens API */
+export interface ReactiveLensesAPI {
+  /** Make the root store */
+  init<S>(s0: S): Store<S>
+
+  /** Make a new store a record of stores */
+  record<R>(stores: {[P in keyof R]: Store<R[P]>}): Store<R>
+
+  /** Get stores for each position currently in the array */
+  each<A>(store: Store<A[]>): Store<A | undefined>[]
+
+  /** Set using an array method (purity is ensured because the spine is copied before running the function) */
+  arr<A, K extends keyof A[]>(store: Store<A[]>, k: K): A[][K]
+
+  /** Lens to a subarray */
+  subarray<A>(bounds: (length: number) => Bounds): Lens<A[], A[]>
+
+  /** Lens to the first N elements */
+  first<A>(N: number): Lens<A[], A[]>,
+
+  /** Lens to the last N elements */
+  last<A>(N: number): Lens<A[], A[]>,
+
+  /** Lens to all but the first N elements */
+  drop<A>(N: number): Lens<A[], A[]>,
+
+  /** Lens to all but the last N elements  */
+  drop_end<A>(N: number): Lens<A[], A[]>,
+
+  /** Lens which paginates a store into equal pieces of a chunk size */
+  paginate<A>(chunk_size: number): Lens<A[], A[][]>
+
+  /** Lens which paginate a store into piece sizes calculated from the page index */
+  paginate<A>(chunk_size: ((i: number) => number)): Lens<A[], A[][]>
+
+  /** Lens which refer to a default value instead of undefined */
+  def<A>(missing: A): Lens<A | undefined, A>
+
+  /** Lens to a particular index in an array */
+  index<A>(position: number): Lens<A[], A | undefined>
+
+  /** Make a lens from a getter and setter */
+  lens<S, T>(get: (s: S) => T, set: (s: S, t: T) => S): Lens<S, T>
+
+  /** Make a lens from an isomorphism
+
+  Note: requires that for all s and t we have f(g(t)) = t and g(f(s)) = s */
+  iso<S, T>(f: (s: S) => T, g: (t: T) => S): Lens<S, T>
+
+  /** Lens to a subfield which must be present. */
+  at<S, K extends keyof S>(k: K): Lens<S, S[K]>
+
+  /** Lens to a particular key in a record.
+
+  Note: the key may be missing from the record.
+  Note: setting the value to undefined removes the key from the record. */
+  key<S, K extends keyof S>(k: K): Lens<S, S[K] | undefined>
+}
+
+class StoreClass<S> implements Store<S> {
   private constructor(
-    /** Start a new transaction: allows using set and modify many times,
-    and only when the (top-level) transaction is finished listeners will be
-    notified. */
     public readonly transaction: (m: () => void) => void,
 
-    /** React on changes. returns an unsubscribe function */
     private readonly listen: (k: () => void) => () => void,
 
-    /** Get the current value
-
-    Note: do not mutate the returned value */
     public readonly get: () => S,
 
-    /** Set to a new value */
     public readonly set: (s: S) => void)
   { }
 
-  /** Make the root store */
   static init<S>(s0: S): Store<S> {
     /** Current state */
     let s = s0
@@ -49,205 +139,255 @@ export class Store<S> {
         pending = true
         notify()
       }
-    return new Store(transaction, k => listeners.push(k), () => s, set)
-
-    interface ListWithRemove<A> {
-      push(a: A): () => void,
-      iter(f: (a: A) => void): void
-    }
-
-    function ListWithRemove<A>(): ListWithRemove<A> {
-      const dict = {} as Record<string, A>
-      let order = [] as (string[] | null)
-      let next_unique = 0
-
-      /** Push a new element, returns the delete function */
-      return {
-        push(a) {
-          const id = next_unique++ + ''
-          dict[id] = a
-          if (order != null) {
-            order.push(id)
-          }
-          return () => {
-            delete dict[id]
-            order = null
-          }
-        },
-        iter(f) {
-          if (order == null) {
-            const cmp = (a: string, b: string) => parseInt(a) - parseInt(b)
-            order = Object.keys(dict).sort(cmp)
-          }
-          order.map(id => {
-            if (id in dict) {
-              f(dict[id])
-            }
-          })
-        }
-      }
-    }
-
+    return new StoreClass(transaction, k => listeners.push(k), () => s, set)
   }
 
-  /** Make a substore with respect to some base store */
-  static sub<B, T>(base: Store<B>, get: () => T, set: (s: T) => void): Store<T> {
-    return new Store(
-      base.transaction,
-      base.listen,
+  modify(f: (s: S) => S): void {
+    return this.set(f(this.get()))
+  }
+
+  on(k: (s: S) => void): () => void {
+    return this.listen(() => k(this.get()))
+  }
+
+  substore<T>(get: () => T, set: (s: T) => void): Store<T> {
+    return new StoreClass(
+      this.transaction,
+      this.listen,
       get,
       set
     )
   }
 
-  /** React on changes. returns an unsubscribe function */
-  on(k: (s: S) => void): () => void {
-    return this.listen(() => k(this.get()))
+  private lens<T>(get: (s: S) => T, set: (s: S, t: T) => S): Store<T> {
+    return this.substore(
+      () => get(this.get()),
+      (t: T) => this.set(set(this.get(), t)))
   }
 
-  /** Modify the value in the store
-
-  Note: return a new value (do not mutate it) */
-  modify(f: (s: S) => S): void {
-    return this.set(f(this.get()))
+  via<T>(lens: Lens<S, T>) {
+    return this.lens(lens.get, lens.set)
   }
 
-  /** Make a new store by projecting a subfield.
-
-  Note: use only when S is actually an object, which always has the key k. */
   at<K extends keyof S>(k: K): Store<S[K]> {
-    return this.lens(s => s[k], (s, v) => ({...(s as any), [k as string]: v}))
-                                            // unsafe cast
-                                                           // safe cast
+    return this.via(at(k))
   }
 
-  /** Make a reference at a particular key in a record.
-
-  Note: the key may be missing from the record.
-  Note: setting the value to undefined removes the key from the record. */
   key<K extends keyof S>(k: K): Store<S[K] | undefined> {
-    return this.lens(
-      x => x[k],
-      (s, v) => {
-        // as string: safe cast
-        // as any: https://github.com/Microsoft/TypeScript/issues/14409
-        if (v == undefined) {
-          const {[k as string]: _, ...s2} = s as any
-          return s2
-        } else {
-          return {
-            ...(s as any),
-            [k as string]: v
-          }
+    return this.via(key(k))
+  }
+}
+
+function lens<S, T>(get: (s: S) => T, set: (s: S, t: T) => S): Lens<S, T> {
+  return {get, set}
+}
+
+function iso<S, T>(f: (s: S) => T, g: (t: T) => S): Lens<S, T> {
+  return lens(f, (_s: S, t: T) => g(t))
+}
+
+function at<S, K extends keyof S>(k: K): Lens<S, S[K]> {
+  return lens(
+    s => s[k],
+    (s, v) => ({...(s as any), [k as string]: v}))
+                // unsafe cast // safe cast
+}
+
+function key<S, K extends keyof S>(k: K): Lens<S, S[K] | undefined> {
+  return lens(
+    x => x[k],
+    (s, v) => {
+      // as string: safe cast
+      // as any: https://github.com/Microsoft/TypeScript/issues/14409
+      if (v == undefined) {
+        const {[k as string]: _, ...s2} = s as any
+        return s2
+      } else {
+        return {
+          ...(s as any),
+          [k as string]: v
         }
       }
-    )
-  }
+    }
+  )
+}
 
-  /** Transform a store via an isomorphism
-
-  Note: requires that for all s and t we have f(g(t)) = t and g(f(s)) = s */
-  iso<T>(f: (s: S) => T, g: (t: T) => S): Store<T> {
-    return Store.sub(
-      this,
-      () => f(this.get()),
-      (t: T) => this.set(g(t))
-    )
-  }
-
-  /** Make a derived store */
-  lens<T>(project: (s: S) => T, inject: (s: S, t: T) => S): Store<T> {
-    return this.iso(project, t => inject(this.get(), t))
-  }
-
-
-/** Refer to a default value instead of undefined */
-static def<A>(store: Store<A | undefined>, missing: A): Store<A> {
-  return store.iso(
+function def<A>(missing: A): Lens<A | undefined, A> {
+  return iso(
     a => a === undefined ? missing : a,
     a => a === missing ? undefined : a)
 }
 
-  /** Make a new reference from many in a record */
-  static record<R>(stores: {[P in keyof R]: Store<R[P]>}): Store<R> {
-    for (const base_key in stores) {
-      const store = stores[base_key]
-      return Store.sub(
-        store,
-        () => {
-          const ret = {} as R
+function record<R>(stores: {[P in keyof R]: Store<R[P]>}): Store<R> {
+  for (const base_key in stores) {
+    const store = stores[base_key]
+    return store.substore(
+      () => {
+        const ret = {} as R
+        for (let k in stores) {
+          ret[k] = stores[k].get()
+        }
+        return ret
+      },
+      (v: R) => {
+        store.transaction(() => {
           for (let k in stores) {
-            ret[k] = stores[k].get()
+            stores[k].set(v[k])
           }
-          return ret
-        },
-        (v: R) => {
-          store.transaction(() => {
-            for (let k in stores) {
-              stores[k].set(v[k])
-            }
-          })
-        }
-      )
-    }
-    throw "Empty record"
+        })
+      }
+    )
   }
+  throw "Empty record"
+}
 
-  /** Make a reference to a particular index in an array */
-  static index<A>(store: Store<A[]>, position: number): Store<A | undefined> {
-    return store.lens(
-      xs => xs[position],
-      (xs, x) => {
-        if (position < xs.length) {
-          const a = xs.slice(0, position)
-          const z = xs.slice(position + 1)
-          if (x === undefined) {
-            return inplace_rtrim([...a, ...z])
-          } else {
-            return inplace_rtrim([...a, x, ...z])
-          }
+function index<A>(position: number): Lens<A[], A | undefined> {
+  return lens(
+    xs => xs[position],
+    (xs, x) => {
+      if (position < xs.length) {
+        const a = xs.slice(0, position)
+        const z = xs.slice(position + 1)
+        if (x === undefined) {
+          return inplace_rtrim([...a, ...z])
         } else {
-          const ys = xs.slice()
-          if (x !== undefined) {
-            // pre-fill with undefined:
-            while (ys.length < position) {
-              ys.push(undefined as any as A)
-            }
-            ys.push(x)
-          }
-          return inplace_rtrim(ys)
+          return inplace_rtrim([...a, x, ...z])
         }
+      } else {
+        const ys = xs.slice()
+        if (x !== undefined) {
+          // pre-fill with undefined:
+          while (ys.length < position) {
+            ys.push(undefined as any as A)
+          }
+          ys.push(x)
+        }
+        return inplace_rtrim(ys)
       }
-    )
-    function inplace_rtrim(ys: A[]): A[] {
-      while (ys.length > 0 && ys[ys.length - 1] === undefined) {
-        ys.pop()
-      }
-      return ys
     }
-  }
-
-  /** Get stores to each position currently in the array */
-  static each<A>(store: Store<A[]>): Store<A | undefined>[] {
-    return store.get().map((_, i) => Store.index(store, i))
-  }
-
-  /** Paginate a store into equal pieces of a chunk size, which is either constant or calculated from the page index */
-  static paginate<A>(store: Store<A[]>, chunk_size: number | ((i: number) => number)): Store<A[][]> {
-    return store.iso(
-      chunk,
-      xss => ([] as A[]).concat(...xss)
-    )
-    function chunk<A>(xs: A[]): A[][] {
-      const out = [] as A[][]
-      const f = typeof chunk_size == 'number' ? (_: number) => chunk_size : chunk_size
-      for (let i = 0, j = 0; i < xs.length; j++) {
-        const n = f(j)
-        out.push(xs.slice(i, i + n))
-        i += n
-      }
-      return out
+  )
+  function inplace_rtrim(ys: A[]): A[] {
+    while (ys.length > 0 && ys[ys.length - 1] === undefined) {
+      ys.pop()
     }
+    return ys
   }
 }
 
+function each<A>(store: Store<A[]>): Store<A | undefined>[] {
+  return store.get().map((_, i) => store.via(index(i)))
+}
+
+function paginate<A>(chunk_size: number | ((i: number) => number)): Lens<A[], A[][]> {
+  return iso(
+    chunk,
+    xss => ([] as A[]).concat(...xss)
+  )
+  function chunk<A>(xs: A[]): A[][] {
+    const out = [] as A[][]
+    const f = typeof chunk_size == 'number' ? (_: number) => chunk_size : chunk_size
+    for (let i = 0, j = 0; i < xs.length; j++) {
+      const n = f(j)
+      out.push(xs.slice(i, i + n))
+      i += n
+    }
+    return out
+  }
+}
+
+function arr<A, K extends keyof Array<A>>(store: Store<Array<A>>, k: K): Array<A>[K] {
+  return (...args: any[]) => {
+    const xs = store.get().slice()
+    const ret = (xs[k] as any)(...args)
+    store.set(xs)
+    return ret
+  }
+}
+
+export interface Bounds {
+  begin: number,
+  end: number
+}
+
+function subarray<A>(bounds: (length: number) => Bounds): Lens<A[], A[]> {
+  return lens(
+    xs => {
+      const {begin, end} = bounds(xs.length)
+      return xs.slice(begin, end)
+    },
+    (xs, ys) => {
+      const {begin, end} = bounds(xs.length)
+      const zs = xs.slice()
+      zs.splice(begin, end - begin, ...ys)
+      return zs
+    }
+  )
+}
+
+const bounded =
+  (l: number, x: number, u: number) =>
+  Math.max(Math.min(l, u), Math.min(x, Math.max(l, u)))
+
+const bounds =
+  (n: number, begin: number, end: number) => ({
+    begin: bounded(0, begin, n),
+    end: bounded(0, end, n)
+  })
+
+
+export const Store: ReactiveLensesAPI = {
+  init: StoreClass.init,
+  paginate,
+  def,
+  record,
+  index,
+  each,
+  arr,
+  subarray,
+  first: (N: number) => subarray((n: number) => bounds(n, 0, N)),
+  last: (N: number) => subarray((n: number) => bounds(n, n-N, n)),
+  drop: (N: number) => subarray((n: number) => bounds(n, N, n)),
+  drop_end: (N: number) => subarray((n: number) => bounds(n, 0, n-N)),
+  lens,
+  iso,
+  at,
+  key
+}
+
+interface ListWithRemove<A> {
+  /** Push a new element, returns the delete function */
+  push(a: A): () => void,
+  /** Iterate over the elements */
+  iter(f: (a: A) => void): void
+}
+
+function ListWithRemove<A>(): ListWithRemove<A> {
+  const dict = {} as Record<string, A>
+  let order = [] as (string[] | null)
+  let next_unique = 0
+
+  return {
+    push(a) {
+      const id = next_unique++ + ''
+      dict[id] = a
+      if (order != null) {
+        order.push(id)
+      }
+      return () => {
+        delete dict[id]
+        order = null
+      }
+    },
+    iter(f) {
+      if (order == null) {
+        const cmp = (a: string, b: string) => parseInt(a) - parseInt(b)
+        order = Object.keys(dict).sort(cmp)
+      }
+      order.map(id => {
+        if (id in dict) {
+          f(dict[id])
+        }
+      })
+    }
+  }
+}
--- test/reactive-lens.test.ts
+++ test/reactive-lens.test.ts
@@ -1,4 +1,4 @@
-import { Store } from './../src/reactive-lens'
+import { Store, Store as L } from './../src/reactive-lens'
 import * as test from "tape"
 
 function reverse<A>(xs: A[]): A[] {
@@ -7,8 +7,7 @@ function reverse<A>(xs: A[]): A[] {
 
 /** Refer to two arrays after each other */
 function glue<A>(a: Store<A[]>, b: Store<A[]>): Store<A[]> {
-  return Store.sub(
-    a,
+  return a.substore(
     () => ([] as A[]).concat(a.get(), b.get()),
     (v: A[]) => {
       const al = a.get().length
@@ -47,7 +46,7 @@ test('reactive-lens', assert => {
 
   const r_a = store.at('a')
   const r_c_e = store.at('c').at('e')
-  const r_a_and_c_e = Store.record({a: r_a, e: r_c_e})
+  const r_a_and_c_e = L.record({a: r_a, e: r_c_e})
 
   r_a.set(999)
   after('set', {a: 999, b: [2, 3], c: {d: [3, 4], e: 4}})
@@ -61,16 +60,16 @@ test('reactive-lens', assert => {
   after('record set', {a: 10, b: [2, 3], c: {d: [3, 4], e: 20}})
 
   const r_bs = store.at('b')
-  Store.each(r_bs)[1].set(882)
+  L.each(r_bs)[1].set(882)
   after('each set', {a: 10, b: [2, 882], c: {d: [3, 4], e: 20}})
 
-  Store.def(Store.index(r_bs, 0), 0).modify(x => x + 1)
+  r_bs.via(L.index(0)).via(L.def(0)).modify(x => x + 1)
   after('index modify', {a: 10, b: [3, 882], c: {d: [3, 4], e: 20}})
 
-  Store.each(glue(r_bs, store.at('c').at('d'))).map(r => Store.def(r, 0).modify(x => x + 1))
+  L.each(glue(r_bs, store.at('c').at('d'))).map(r => r.via(L.def(0)).modify(x => x + 1))
   after('glue', {a: 10, b: [4, 883], c: {d: [4, 5], e: 20}}, 4)
 
-  Store.index(r_bs.iso(reverse, reverse), 0).set(42)
+  r_bs.via(L.iso(reverse, reverse)).via(L.index(0)).set(42)
   after('iso reverse', {a: 10, b: [4, 42], c: {d: [4, 5], e: 20}})
 
   let a: any
@@ -92,25 +91,25 @@ test('reactive-lens', assert => {
 
 test('array removal', assert => {
   const {store, after} = init([0,1,2], assert)
-  Store.index(store, 1).set(undefined)
+  store.via(L.index(1)).set(undefined)
   after('removing 1', [0,2])
-  Store.def(Store.index(store, 1), 0).set(0)
+  store.via(L.index(1)).via(L.def(0)).set(0)
   after('removing 2 via def', [0])
   assert.end()
 })
 
 test('array insertion', assert => {
   const {store, after} = init([0,1,2], assert)
-  Store.index(store, 3).set(3)
+  store.via(L.index(3)).set(3)
   after('inserting 3', [0,1,2,3])
-  Store.def(Store.index(store, 4), 0).set(4)
+  store.via(L.index(4)).via(L.def(0)).set(4)
   after('inserting 4 with def', [0,1,2,3,4])
   assert.end()
 })
 
 test('array insertion far out', assert => {
   const {store, after} = init([0,1,2], assert)
-  const r4 = Store.index(store, 4)
+  const r4 = store.via(L.index(4))
   r4.set(4)
   after('inserting 4', [0,1,2,void 0,4])
   r4.set(undefined)
@@ -127,9 +126,9 @@ test('key', assert => {
   after('setting apa', {apa: 3, bepa: 2})
   apa.set(undefined)
   after('removing apa', {bepa: 2})
-  Store.def(bepa, 0).set(0)
+  bepa.via(L.def(0)).set(0)
   after('removing bepa via def', {})
-  Store.def(cepa, 0).set(3)
+  cepa.via(L.def(0)).set(3)
   after('inserting cepa via def', {cepa: 3})
   assert.is(cepa.get(), 3, 'get')
   assert.is(apa.get(), undefined, 'get missing')
@@ -139,28 +138,82 @@ test('key', assert => {
 test('paginate', assert => {
   const {store, after} = init([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], assert)
 
-  const p = Store.paginate(store, 3)
-  Store.def(Store.index(p, 1), []).modify(xs => xs.map(x => -x))
+  const empty_default = L.def([] as number[])
+
+  const p = store.via(L.paginate(3))
+  p.via(L.index(1)).via(empty_default).modify(xs => xs.map(x => -x))
   after('paginated modify', [1, 2, 3, -4, -5, -6, 7, 8, 9, 10, 11])
 
-  Store.def(Store.index(p, 3), []).modify(xs => xs.map(x => -x))
+  p.via(L.index(3)).via(empty_default).modify(xs => xs.map(x => -x))
   after('paginated chopped modify', [1, 2, 3, -4, -5, -6, 7, 8, 9, -10, -11])
 
-  Store.def(Store.index(p, 1), []).modify(xs => [])
+  p.via(L.index(1)).via(empty_default).modify(xs => [])
   after('paginated remove', [1, 2, 3, 7, 8, 9, -10, -11])
 
   store.transaction(() => {
-    Store.each(Store.def(Store.index(p, 1), [])).map(r => r.modify(x => x === undefined || x > 8 ? undefined : x))
+    L.each(p.via(L.index(1)).via(empty_default)).map(r => r.modify(x => x === undefined || x > 8 ? undefined : x))
   })
   after('paginated filtering', [1, 2, 3, 7, 8, -10, -11])
 
   store.set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
   assert.deepEqual(
-    Store.paginate(store, i => i).get(),
+    store.via(L.paginate(i => i)).get(),
     [[], [0], [1, 2], [3, 4, 5], [6, 7, 8, 9]],
     'triangulate'
   )
 
+  assert.end()
+})
+
+test('arr', assert => {
+  const {store, after} = init([0,1,2,3,4], assert)
+  assert.deepEqual(L.arr(store, 'splice')(1,3,9,10), [1,2,3], 'return value')
+  after('splicing', [0,9,10,4])
+  assert.end()
+})
+
+test('subarray', assert => {
+  const {store, after} = init([0,1,2,3,4], assert)
+  assert.deepEqual(store.via(L.first(2)).get(), [0,1], 'first 2')
+  assert.deepEqual(store.via(L.drop(2)).get(), [2,3,4], 'drop 2')
+  assert.deepEqual(store.via(L.last(2)).get(), [3,4], 'last 2')
+  assert.deepEqual(store.via(L.drop_end(2)).get(), [0,1,2], 'drop_end 2')
+
+  assert.deepEqual(store.via(L.first(6)).get(), [0,1,2,3,4], 'first 6')
+  assert.deepEqual(store.via(L.drop(6)).get(), [], 'drop 6')
+  assert.deepEqual(store.via(L.last(6)).get(), [0,1,2,3,4], 'last 6')
+  assert.deepEqual(store.via(L.drop_end(6)).get(), [], 'drop_end 6')
+
+  const f2 = store.via(L.first(2))
+  const d2 = store.via(L.drop(2))
+  const l2 = store.via(L.last(2))
+  const d2e = store.via(L.drop_end(2))
+  f2.set([9,8,7])
+  after('set first 2', [9,8,7,2,3,4])
+  f2.set([])
+  after('set first 2', [7,2,3,4])
+  d2.set([9,8,7])
+  after('set drop 2', [7,2,9,8,7])
+  d2.set([])
+  after('set drop 2', [7,2])
+  f2.set([])
+  after('set first 2', [])
+  d2.set([1000])
+  after('set drop 2', [1000])
+  store.set([1,2,3,4,5])
+  after('reset', [1,2,3,4,5])
+  l2.set([])
+  after('set last 2 end', [1,2,3])
+  l2.set([8,9,1000])
+  after('set last 2 end', [1,8,9,1000])
+  d2e.set([999])
+  after('set drop 2 end', [999,9,1000])
+  d2e.set([])
+  after('set drop 2 end', [9,1000])
+  store.set([1,2,3,4,5])
+  after('reset', [1,2,3,4,5])
+  d2e.set([])
+  after('set drop 2 end', [4,5])
   assert.end()
 })
 
